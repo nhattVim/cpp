55ed0fa88905bad42aa96f0c3bff27e6e351017f
nhattruongNeoVim <nhattruong13112000@gmail.com>
2024-03-30 21:13:13 +0700

    update

--- Exercise/Homeworks/hw4/hw4.cpp
+++ Exercise/Homeworks/hw4/hw4.cpp
@@ -87,11 +87,24 @@ SLLStudents *rankStudent (SLLStudents *p) {
 }
 
 SLLStudents *findByName (SLLStudents *p, string name) {
-    SLLStudents *q = new SLLStudents;
     while (p != nullptr) {
         if (p -> data.name == name) {
-        
+            return p;
         }
+        p = p -> next;
+    }
+    return nullptr;
+}
+
+void deleteSLL (SLLStudents *&head, SLLStudents *x) {
+    while (head != nullptr) {
+        if (head == x) {
+            SLLStudents *p = x;
+            p = head -> next;
+            head -> next = p -> next;
+            delete p;
+        }
+        head = head -> next;
     }
 }
 
@@ -120,8 +133,17 @@ int main (int argc, char *argv[]) {
     printSLL(rank);
     cout << "\n\n";
 
-    // d)
-    findByName(p, "test");
+    // f)
+    SLLStudents *f = findByName(p, "Nguyễn Văn Test");
+    if (f != nullptr) {
+        cout << "Đã tìm thấy sinh viên: " << f -> data.name << ", " << f -> data.age << ", " << f -> data.gpa << "\n\n";
+    } else {
+        cout << "Không tìm thấy sinh viên có tên này.\n\n";
+    }
+
+    // g)
+    deleteSLL(p, findByName(p, "Nguyễn Văn Tèo"));
+    printSLL(p);
 
     return 0;
 }
