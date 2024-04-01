f3fa60290ca8ba98b6265457a9528bfbfe08579b
nhattruongNeoVim <nhattruong13112000@gmail.com>
2024-04-01 17:44:06 +0700

    update

--- Exercise/Homeworks/hw4/hw4.cpp
+++ Exercise/Homeworks/hw4/hw4.cpp
@@ -96,15 +96,97 @@ SLLStudents *findByName (SLLStudents *p, string name) {
     return nullptr;
 }
 
-void deleteSLL (SLLStudents *&head, SLLStudents *x) {
-    while (head != nullptr) {
-        if (head == x) {
-            SLLStudents *p = x;
-            p = head -> next;
-            head -> next = p -> next;
+void deleteSLL(SLLStudents *&head, SLLStudents *x) {
+    SLLStudents *p1 = nullptr;
+    SLLStudents *p = head;
+
+    while (p != nullptr) {
+        if (p == x) {
+            if (p1 == nullptr) {
+                head = p -> next;
+            } else {
+                p1 -> next = p -> next;
+            }
+            cout << "Delete " << p -> data.name << " successfully" << "\n\n";
             delete p;
+            break;
         }
+        p1 = p;
+        p = p->next;
+    }
+}
+
+void insertAtPosition(SLLStudents *&head, int k, Student newStudent) {
+    SLLStudents *newNode = new SLLStudents;
+    newNode -> data = newStudent;
+    newNode -> next = nullptr;
+
+    if (k == 0) {
+        newNode -> next = head;
+        head = newNode;
+        return;
+    }
+
+    SLLStudents *temp = head;
+    for (int i = 0; temp != nullptr && i < k - 1; ++i) {
+        temp = temp -> next;
+    }
+
+    if (temp == nullptr) {
+        cout << "Vị trí k không hợp lệ\n";
+        delete newNode;
+        return;
+    }
+
+    newNode -> next = temp -> next;
+    temp -> next = newNode;
+}
+
+void deleteAtPosition(SLLStudents *&head, int k) {
+    if (head == nullptr) {
+        cout << "Danh sách rỗng\n";
+        return;
+    }
+
+    if (k == 0) {
+        SLLStudents *temp = head;
         head = head -> next;
+        delete temp;
+        return;
+    }
+
+    SLLStudents *p = head;
+    for (int i = 0; p != nullptr && i < k - 1; ++i) {
+        p = p -> next;
+    }
+
+    if (p == nullptr || p -> next == nullptr) {
+        cout << "Vị trí k không hợp lệ\n";
+        return;
+    }
+
+    SLLStudents *tmp = p -> next;
+    p -> next = tmp -> next;
+    delete tmp;
+}
+
+void sortByGPA(SLLStudents *&head) {
+    SLLStudents *p = head;
+    while (p != nullptr) {
+        SLLStudents *max = p;
+        SLLStudents *maxNext = p -> next;
+        while (maxNext != nullptr) {
+            if (maxNext -> data.gpa > max -> data.gpa) {
+                max = maxNext;
+            }
+            maxNext = maxNext->next;
+        }
+
+        if (max != p) {
+            swap(p -> data, max -> data);
+        }
+
+        p = p -> next;
     }
 }
 
@@ -142,8 +224,28 @@ int main (int argc, char *argv[]) {
     }
 
     // g)
-    deleteSLL(p, findByName(p, "Nguyễn Văn Tèo"));
+    deleteSLL(p, findByName(p, "Nguyễn Văn Test"));
     printSLL(p);
+    cout << "\n\n";
+
+    // h)
+    Student newStudent;
+    newStudent.name = "Nguyễn Văn New";
+    newStudent.age = 20;
+    newStudent.gpa = 3.5;
+    insertAtPosition(p, 2, newStudent);
+    printSLL(p);
+    cout << "\n\n";
+
+    // i)
+    deleteAtPosition(p, 2);
+    printSLL(p);
+    cout << "\n\n";
+
+    // j)
+    sortByGPA(p);
+    printSLL(p);
+    cout << "\n\n";
 
     return 0;
 }
